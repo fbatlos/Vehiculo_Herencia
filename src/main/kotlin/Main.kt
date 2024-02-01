8a884dcf1453d0dbc6750483ae15aedd7b314a72
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-02-01 21:32:39 +0100

    Herencia acabada de Vehiculo , puedes tomarlo de ejemplo.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,10 +33,12 @@ class Motocicleta(var cilindrada:Int,marca:String, modelo:String, capacidadCombi
 }
 
 fun main(){
-    val coche1=Automovil(tipos.SUV , "wosl" , "23z", 10)
+  /*  val coche1=Automovil(tipos.SUV , "wosl" , "23z", 10)
     val moto = Motocicleta(2,"kuzuaki","5M4",10)
     println(coche1.toString())
     println(moto.toString())
-
+    */
+   // val coche :Automovil = Vehiculo("Hoal" , "637Z" , 10)
+    //println(coche.toString())
 }
 
